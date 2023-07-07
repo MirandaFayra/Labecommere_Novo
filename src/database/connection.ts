5055aa6edd49e07138ad7188605379567a3d2d95
Fayra Miranda <52434685+MirandaFayra@users.noreply.github.com>
2023-07-07 19:33:38 -0300

    Fix: Ajustar caminho da conexcao do banco de dados

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -3,7 +3,7 @@ import { knex } from "knex"
 export const db = knex({
     client: "sqlite3",
     connection: {
-        filename: "./src/database/teste.db", 
+        filename: "./src/database/database.db", 
     },
     useNullAsDefault: true, 
     pool: {
