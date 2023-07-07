ae0d16aad6e0b033d77811ea13f40ed57a0f4eb6
Fayra Miranda <52434685+MirandaFayra@users.noreply.github.com>
2023-07-07 19:57:22 -0300

    Finalizar endpoint de get all users

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from 'express'
 import cors from 'cors'
+import {db} from './database/connection'
 
 const app = express()
 
@@ -15,10 +16,22 @@ app.get('/ping', (req: Request, res: Response) => {
 
 app.get('/users',async (req: Request, res: Response)=> {
     try {
-        const result = await 
-        
+        const result = await db.select("*").from('users');
+        res.status(200).send(result)
+
     } catch (error) {
-        
+        console.log(error)
+
+        if(req.statusCode === 200){
+            res.status(500).send("Desculpe, mas parece que ocorreu um erro interno. Por favor, tente novamente mais tarde")
+        }
+
+        //Tratamento apenas de erros reais
+        if(error instanceof Error) {
+            res.send(error.message)
+        }else{
+            res.send("Erro inesperado")
+        }     
     }
 })
 
