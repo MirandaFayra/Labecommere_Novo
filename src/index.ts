d3120611c8c2857a5eaef7e2df393ec99ea5159a
Fayra Miranda <52434685+MirandaFayra@users.noreply.github.com>
2023-07-07 21:08:26 -0300

    Finalizar o endpoint de create product

--- src/index.ts
+++ src/index.ts
@@ -75,7 +75,7 @@ app.post("/users",async(req:Request,res:Response)=>{
 
         await db ("users").insert (newUser)
 
-        res.status(200).send("Banda cadastrada com sucesso")
+        res.status(200).send("Pessoa usuária cadastrada com sucesso")
         
     } catch (error) {
         console.log(error)
@@ -94,11 +94,72 @@ app.post("/users",async(req:Request,res:Response)=>{
 
 //------------- CREATE PRODUCT ------------
 
+app.post("/products",async(req:Request,res:Response)=>{
+    try {
+        const {id,name,price,description,image_url} =req.body
+        
+        if(typeof id !== "string"){
+            res.status(400)
+            throw new Error("'id' inválido, deve ser string")
+        }
+
+        if(typeof name !== "string"){
+            res.status(400)
+            throw new Error("'name' inválido, deve ser string")
+        }
+
+        if(typeof price !== "number"){
+            res.status(400)
+            throw new Error("'price' inválido, deve ser um número")
+        }
+
+        if(typeof description !== "string"){
+            res.status(400)
+            throw new Error("'description' inválido, deve ser string")
+        }
+
+        if(typeof image_url !== "string"){
+            res.status(400)
+            throw new Error("'image_url' inválido, deve ser string")
+        }
+
+        if (id.length < 1 || name.length < 1 ||description.length < 1||image_url.length < 1) {
+            res.status(400)
+            throw new Error("'id', 'name', 'description', 'image_url' devem possuir no mínimo 1 caractere")
+        }
+
+        const newProduct ={
+            id,
+            name,
+            price,
+            description,
+            image_url
+        }
+
+        await db ("products").insert(newProduct)
+
+        res.status(200).send("Pessoa usuária cadastrada com sucesso")
+        
+    } catch (error) {
+        console.log(error)
+
+        if(req.statusCode === 200){
+            res.status(500).send("Desculpe, mas parece que ocorreu um erro interno. Por favor, tente novamente mais tarde")
+        }
+
+        if(error instanceof Error) {
+            res.send(error.message)
+        }else{
+            res.send("Erro inesperado")
+        }   
+    }
+})
+
 // ------------ GET ALL PRODUCTS ------------
 
 //------------ EDIT PRODUCT BY ID -----------
 
-//------------ CREATE PRODUCT --------------
+//------------ CREATE PURCHASE --------------
 
 //------------ DELETE PURCHASE BY ID ----------
 
