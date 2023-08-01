a4d810c6152dd692b85abfe72b135fea7677c6b8
Fayra Miranda <52434685+MirandaFayra@users.noreply.github.com>
2023-08-01 13:38:34 -0300

    Finalizar refatoramento do endpoint getAllProducts

---
+++ src/endpoints/products/getAllProducts.ts
@@ -0,0 +1,29 @@
+import { Request, Response } from "express"
+import { db } from "../../database/connection"
+
+export const getAllProducts = async (req:Request, res:Response) => {
+    try {
+        const name = req.query.name
+
+        if (name === undefined) {
+            const productsResult = await db.select("*").from("products");
+            res.status(200).send(productsResult)
+        }else{
+            const productsFilteredByName = await db("products").where("name", "LIKE", `%${name}%`)
+            res.status(200).send(productsFilteredByName)
+        }
+        
+    } catch (error) {
+        console.log(error)
+
+        if(req.statusCode === 200){
+            res.status(500).send("Desculpe, mas parece que ocorreu um erro interno. Por favor, tente novamente mais tarde")
+        }
+
+        if(error instanceof Error) {
+            res.send(error.message)
+        }else{
+            res.send("Erro inesperado")
+        }  
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import {db} from './database/connection'
 import { creatNewUser } from './endpoints/users/createNewUser'
 import { getAllUsers } from './endpoints/users/getAllUsers'
 import { createNewProduct } from './endpoints/products/createNewProduct'
+import { getAllProducts } from './endpoints/products/getAllProducts'
 
 const app = express()
 
@@ -11,121 +12,13 @@ app.use(express.json())
 app.use(cors())
 
 //------------- USERS ------------
-
 app.get("/users",getAllUsers)
 app.post("/users",creatNewUser)
 
-//------------- CREATE PRODUCT ------------
+//-------------PRODUCTS ------------
+app.get("/products",getAllProducts)
 app.post("/products",createNewProduct)
 
-/*app.post("/products",async(req:Request,res:Response)=>{
-    try {
-        const {id,name,price,description,image_url} =req.body
-        
-        if(typeof id !== "string"){
-            res.status(400)
-            throw new Error("'id' inválido, deve ser string")
-        }
-
-        if(typeof name !== "string"){
-            res.status(400)
-            throw new Error("'name' inválido, deve ser string")
-        }
-
-        if(typeof price !== "number"){
-            res.status(400)
-            throw new Error("'price' inválido, deve ser um número")
-        }
-
-        if(typeof description !== "string"){
-            res.status(400)
-            throw new Error("'description' inválido, deve ser string")
-        }
-
-        if(typeof image_url !== "string"){
-            res.status(400)
-            throw new Error("'image_url' inválido, deve ser string")
-        }
-
-        if (id.length < 1 || name.length < 1 ||description.length < 1||image_url.length < 1) {
-            res.status(400)
-            throw new Error("'id', 'name', 'description', 'image_url' devem possuir no mínimo 1 caractere")
-        }
-
-        const newProduct ={
-            id,
-            name,
-            price,
-            description,
-            image_url
-        }
-
-        await db ("products").insert(newProduct)
-
-        res.status(200).send("Pessoa usuária cadastrada com sucesso")
-        
-    } catch (error) {
-        console.log(error)
-
-        if(req.statusCode === 200){
-            res.status(500).send("Desculpe, mas parece que ocorreu um erro interno. Por favor, tente novamente mais tarde")
-        }
-
-        if(error instanceof Error) {
-            res.send(error.message)
-        }else{
-            res.send("Erro inesperado")
-        }   
-    }
-})*/
-
-//-------------- GET ALL PRODUCTS FUN 1 E FUN 2 ------------------
-
-app.get("/products",async (req:Request, res:Response) => {
-    try {
-        const name = req.query.name;
-        //const productsResult = await db.select("*").from("products");
-        //const productsFilteredByName = await db.select("*").from("products").where("name", "LIKE", `%${name}%`);
-        //const productsFilteredByName = await db.raw(
-            //`SELECT * FROM products 
-            //WHERE name LIKE '%${name}%'`)
-
-           // const productsResult = await db.select("*").from("products");
-
-            //const productsFiltred = productsResult.filter(product => product.name === name)
-
-
-        /*if(name){
-            const productsFilteredByName = await db.select("*").from("products").where("name", "LIKE", `%${name}%`);
-            console.log("cai aqui")
-            res.status(200).send(productsFilteredByName)
-
-        }else{
-            const productsResult = await db.select("*").from("products");
-            res.status(200).send(productsResult)
-        }*/
-        if (name === undefined) {
-            const productsResult = await db.select("*").from("products");
-            res.status(200).send(productsResult)
-        }else{
-            const productsFilteredByName = await db("products").where("name", "LIKE", `%${name}%`)
-            res.status(200).send(productsFilteredByName)
-        }
-        
-    } catch (error) {
-        console.log(error)
-
-        if(req.statusCode === 200){
-            res.status(500).send("Desculpe, mas parece que ocorreu um erro interno. Por favor, tente novamente mais tarde")
-        }
-
-        if(error instanceof Error) {
-            res.send(error.message)
-        }else{
-            res.send("Erro inesperado")
-        }  
-    }
-})
 //------------ EDIT PRODUCT BY ID -----------
 app.put("/products/:id",async(req:Request,res:Response)=>{
     try {
